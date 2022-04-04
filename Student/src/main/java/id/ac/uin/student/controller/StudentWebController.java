07c368fc08dd0b99cb6b17bb7cc1e8b1d54d4bf1
munazir dzuana <munazir805@gmail.com>
2022-04-04 21:17:58 +0700

    #4 bisa crud tapi notifikasi error tidak keluar

---
+++ Student/src/main/java/id/ac/uin/student/controller/StudentWebController.java
@@ -0,0 +1,56 @@
+package id.ac.uin.student.controller;
+
+import id.ac.uin.student.entity.Student;
+import id.ac.uin.student.service.StudenServices;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@AllArgsConstructor
+public class StudentWebController {
+    private StudenServices studentService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("student", studentService.getAllStudents());
+        return "index";
+    }
+
+    @GetMapping(value = "/create")
+    public String create1(Model model) {
+        model.addAttribute("student", new Student());
+        return "formStudent";
+    }
+
+    @PostMapping(value = "/create")
+    public String tambahStudent(@Valid Model model, Student student, Errors error) {
+        if(null != error && error.getErrorCount()>0){
+            return "index";
+        }
+        else {
+            model.addAttribute("student",studentService.save(student));
+            model.addAttribute("succes","berhasil disimpan");
+            return "succes";
+        }
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String editForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.findById(id));
+        return "formStudent";
+    }
+
+    @GetMapping(value = "/hapus/{id}")
+    public String hapusStudent(@PathVariable Long id) {
+        studentService.deleteById(id);
+        return "redirect:/";
+    }
+}
--- Student/src/main/java/id/ac/uin/student/entity/Student.java
+++ Student/src/main/java/id/ac/uin/student/entity/Student.java
@@ -1,9 +1,12 @@
 package id.ac.uin.student.entity;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
 
 @Data
 @NoArgsConstructor
@@ -12,26 +15,38 @@ import javax.validation.constraints.NotEmpty;
 @Table(name = "student")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @NotEmpty(message = "First Name is required")
-    @Column(nullable = false, name = "firstName")
+    @NotNull(message = "First Name Cant be null")
+    @Column(nullable = false, name = "first_Name")
     private String firstName;
 
     @NotEmpty(message = "Last Name is required")
-    @Column(nullable = false, name = "lastName")
+    @NotNull(message = "Last Name Cant be null")
+    @Column(nullable = false, name = "last_Name")
     private String lastName;
 
     @NotEmpty(message = "Email is required")
+    @NotNull(message = "Email Cant be null")
     @Column(nullable = false, unique = true, name = "email")
+    @Email
     private String email;
 
     @NotEmpty(message = "Phone Number is required")
+    @NotNull(message = "Phone Cant be null")
+    @Size(min = 10, max = 13, message = "phone Number should be of 10 digits")
     @Column(nullable = false, unique = true, name = "phone")
     private String phoneNumber;
 
     @NotEmpty(message = "Jurusan is required")
+    @NotNull(message = "Jurusan Cant be null")
     @Column(nullable = false, name = "jurusan")
     private String jurusan;
+
+    @NotNull(message = "Please enter birth date")
+    @Past (message = "Birth date should be less than current date!!")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate birthDay;
 }
